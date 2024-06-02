6c9b196ce091d965a4f41e6b63f08d96b6e7ac61
OnePuchPotato <73746820+Jibek1906@users.noreply.github.com>
2024-06-02 16:47:33 +0600

    Engish/Russian versions

--- Meet_Conference/meet2024/main/views.py
+++ Meet_Conference/meet2024/main/views.py
@@ -3,12 +3,21 @@ from .forms import RegistrationForm
 from .models import Gallery, SliderImage, IntroductionImage
 
 def index(request):
+    return render_index(request, 'index.html')
+
+def index_en(request):
+    return render_index(request, 'english.html', lang='en')
+
+def render_index(request, template_name, lang='ru'):
     form = RegistrationForm()
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('index')
+            if lang == 'en':
+                return redirect('index_en')
+            else:
+                return redirect('index')
     gallery = Gallery.objects.all()
     slider_images = SliderImage.objects.all()
     introduction_images = IntroductionImage.objects.all()
@@ -18,4 +27,4 @@ def index(request):
         'slider_images': slider_images,
         'introduction_images': introduction_images,
     }
-    return render(request, 'index.html', context)
+    return render(request, template_name, context)
--- Meet_Conference/meet2024/meet2024/urls.py
+++ Meet_Conference/meet2024/meet2024/urls.py
@@ -3,12 +3,11 @@ from django.urls import path
 from main import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import include
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('en/', views.index_en, name='english'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
